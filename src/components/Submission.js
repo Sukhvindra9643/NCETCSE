ccbbbbf66fd197cad0108a2ad53a0ed85016e608
Sukhvindra9643 <20b1029.bca1@gitmgurgaon.com>
2023-03-28 22:27:26 +0530

    done

---
+++ src/components/Submission.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Submission.css"
+const Submission = () => {
+  return (
+    <div className='submission-container'>
+      <div className='container'>
+        <div className='row'>
+            <div className='structure_paper'></div>
+            <div className='research_paper_format'></div>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Submission
